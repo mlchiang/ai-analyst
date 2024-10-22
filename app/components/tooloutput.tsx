3ad8f66fb3163a55be58726defffd4c28cebfcb5
Mish Ushakov <mishushakov@users.noreply.github.com>
2024-10-22 17:30:13 +0200

    updated interactive chart

--- app/components/tooloutput.tsx
+++ app/components/tooloutput.tsx
@@ -1,13 +1,22 @@
-import { Result } from "@e2b/code-interpreter";
+import {
+  ChartType,
+  LineChart as LineChartType,
+  Result,
+} from "@e2b/code-interpreter";
 import { useState } from "react";
-import { Line } from "@nivo/line";
 import { ToolResult } from "../lib/types";
+import {
+  CartesianGrid,
+  Legend,
+  LineChart,
+  Line,
+  Tooltip,
+  XAxis,
+  YAxis,
+  Label,
+} from "recharts";
 
-export function ToolOutput({
-  result,
-}: {
-  result: ToolResult | undefined;
-}) {
+export function ToolOutput({ result }: { result: ToolResult | undefined }) {
   const [viewMode, setViewMode] = useState<"static" | "interactive">("static");
 
   if (!result) return null;
@@ -15,7 +24,7 @@ export function ToolOutput({
 
   return toolResult !== undefined
     ? toolResult.results.map((result: Result, index: number) => (
-        <div key={index}>
+        <div key={index} className="flex flex-col gap-2">
           <div className="flex justify-end">
             <button
               className={`px-2 border-b font-semibold text-sm ${
@@ -51,22 +60,57 @@ function RenderResult({
     return <img src={`data:image/png;base64,${result.png}`} alt="plot" />;
   }
 
-  if (viewMode === "interactive" && result.chart) {
-    const data = result.chart.elements.map((e) => {
-      return {
-        id: e.label,
-        data: e.points.map((p: [number, number]) => ({ x: p[0], y: p[1] })),
-      };
-    });
+  if (viewMode === "interactive" && result.extra.chart) {
+    const chart = result.extra.chart;
+    if (chart.type === "line") {
+      const data = (chart as LineChartType).elements.map((e) => {
+        return e.points.map((p: [number, number]) => ({
+          x: p[0],
+          y: p[1],
+        }));
+      });
 
-    return (
-      <Line
-        height={500}
-        width={1200}
-        data={data}
-        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-      />
-    );
+      return (
+        <LineChart height={400} width={600} data={data[0]}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="x"
+            scale={chart.x_scale}
+            label={{
+              value: chart.x_label,
+              position: "left",
+              offset: 10,
+            }}
+          />
+          <YAxis
+            label={{
+              value: chart.y_label,
+              position: "left",
+              offset: 0,
+              angle: -90,
+            }}
+            scale={chart.y_scale}
+          />
+          <Tooltip />
+          <Legend
+            verticalAlign="top"
+            height={36}
+            content={
+              <p className="text-sm font-semibold text-gray-800">
+                {chart.title}
+              </p>
+            }
+          />
+          <Line name={chart.y_label} type="monotone" dataKey="y" />
+        </LineChart>
+      );
+    }
+
+    // if (chart.type === "superchart") {
+    //   return chart.elements.map((e) => {
+    //     return <RenderResult result={e} viewMode={viewMode} />;
+    //   });
+    // }
   }
 
   return <pre>{JSON.stringify(result, null, 2)}</pre>;
