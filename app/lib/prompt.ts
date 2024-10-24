d43f3e84d6edfe7977132cd53a2a1b8856eedac3
Mish Ushakov <mishushakov@users.noreply.github.com>
2024-10-24 16:29:22 +0200

    update importing prompt

--- app/lib/prompt.ts
+++ app/lib/prompt.ts
@@ -6,7 +6,7 @@ You are a sophisticated python data scientist/analyst.
 You are provided with a question and a dataset.
 Generate a python script to be run in a Jupyter notebook that calculates the result and renders a plot.
 Only one code block is allowed, use markdown code blocks.
-Install additional packages using !pip syntax.
+Install additional packages (using !pip syntax) before importing them.
 
 The following libraries are already installed:
 - jupyter
